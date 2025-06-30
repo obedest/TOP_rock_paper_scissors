890cf6505f1cd17f1730ecaf5b422fd083546ce8
obedest <161562219+obedest@users.noreply.github.com>
2025-06-30 22:13:09 +1000

    Create js code file with code to get human and computer choices and play one round

---
+++ javascript.js
@@ -0,0 +1,41 @@
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() {
+    const prob = Math.random(); // or let??
+    if (prob < 0.33) {
+        return 'rock';
+    } else if (prob < 0.66) {
+        return 'paper';
+    } else {
+        return 'scissors';
+    }
+}
+
+function getHumanChoice() {
+    const choice = prompt("Hi! Please enter you choice: 'rock', 'paper' or 'scissors'"); // or let??
+    return choice;
+}
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase()
+    if (humanChoice == computerChoice) {
+        return 'Tie!'
+    } else if (
+        (humanChoice == 'rock' && computerChoice == 'scissors') ||
+        (humanChoice == 'paper' && computerChoice == 'rock') ||
+        (humanChoice == 'scissors' && computerChoice == 'paper')
+    ) {
+        return 'You win! ' + humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1) + ' beats ' + computerChoice
+    } else {
+        return 'You lose! ' + computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1) + ' beats ' + humanChoice
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+console.log(playRound(humanSelection, computerSelection));
+
+// console.log(getComputerChoice());
+// console.log(getHumanChoice());
